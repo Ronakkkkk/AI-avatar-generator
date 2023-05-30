688473e34bcdbb87a2ee9724df1f8fb85db331d2
Ronak <ronak01.raj2gmail.com>
2023-05-30 16:32:22 +0530

    displaying images

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,6 +1,13 @@
 const generateAction = async (req, res) => {
     console.log('Received request')
     const input = JSON.parse(req.body).input;
+    const bufferToBase64 = (buffer) => {
+      let arr = new Uint8Array(buffer);
+      const base64 = btoa(
+        arr.reduce((data, byte) => data + String.fromCharCode(byte), '')
+      )
+      return `data:image/png;base64,${base64}`;
+    };
     const response = await fetch(
         `https://api-inference.huggingface.co/models/Ronak0101/momo`,
         {
@@ -17,7 +24,9 @@ const generateAction = async (req, res) => {
 
       if (response.ok) {
         const buffer = await response.arrayBuffer();
-        res.status(200).json({ image: buffer });
+        const base64 = bufferToBase64(buffer);
+        // Make sure to change to base64
+        res.status(200).json({ image: base64 });
       } else if (response.status === 503) {
         const json = await response.json();
         res.status(503).json(json);
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
@@ -10,11 +10,32 @@ const Home = () => {
   const [retry, setRetry] = useState(0);
   // Number of retries left
   const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [finalPrompt, setFinalPrompt] = useState('');
   const onChange = (event) => {
     setInput(event.target.value);
   };
+
   const generateAction = async () => {
     console.log('Generating...');	
+     // Add this check to make sure there is no double click
+  if (isGenerating && retry === 0) return;
+
+  // Set loading has started
+  setIsGenerating(true);
+    
+    // If this is a retry request, take away retryCount
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+
+      setRetry(0);
+    }
     const response = await fetch('/api/generate', {
       method: 'POST',
       headers: {
@@ -26,57 +47,106 @@ const Home = () => {
     const data = await response.json();
 
     if (response.status === 503) {
-      console.log('Model is loading still :(.')
+      setRetry(data.estimated_time);
       return;
     }
   
     // If another error, drop error
     if (!response.ok) {
       console.log(`Error: ${data.error}`);
+      setIsGenerating(false);
+      return;
+    }
+    setFinalPrompt(input);
+    setInput('');
+    setImg(data.Image);
+    setIsGenerating(false);
+  }
+  
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(`Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`);
+        setRetryCount(maxRetries);
+        return;
+        }
+
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+
+      await generateAction();
+    };
+
+    if (retry === 0) {
       return;
     }
 
-    setImg(data.Image);
-  }
+    runRetry();
+  }, [retry]);
+  
   
   return (
     <div className="root">
-      <Head>
-        <title>Silly Picture Generator</title>
-      </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>Silly picture generator</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>Turn me into anyone you want (hulk please!) Make sure you refer to me as "ronka" in the prompt</h2>
-          </div>
-          <div className="prompt-container">
-          <input className="prompt-box" value={input} onChange={onChange}/>
-          <div className="prompt-buttons">
-    <a className="generate-button">
-      <div className="generate" onClick={generateAction}>
-        <p>Generate</p>
+  <Head>
+    <title>Silly picture generator | buildspace</title>
+  </Head>
+  <div className="container">
+    <div className="header">
+      <div className="header-title">
+        <h1>Silly picture generator</h1>
       </div>
-    </a>
-    </div>
+      <div className="header-subtitle">
+        <h2>
+          Turn me into anyone you want! Make sure you refer to me as "ronka" in the prompt
+        </h2>
       </div>
+      <div className="prompt-container">
+        <input className="prompt-box" value={input} onChange={onChange} />
+        <div className="prompt-buttons">
+          <a
+            className={
+              isGenerating ? 'generate-button loading' : 'generate-button'
+            }
+            onClick={generateAction}
+          >
+            <div className="generate">
+              {isGenerating ? (
+                <span className="loader"></span>
+              ) : (
+                <p>Generate</p>
+              )}
+            </div>
+          </a>
         </div>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-avatar"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {/* <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div> */}
-        </a>
-      </div>
     </div>
+    {/* Add output container */}
+    {img && (
+      <div className="output-content">
+        <Image src={img} width={512} height={512} alt={input} />
+        <p>{finalPrompt}</p>
+      </div>
+    )}
+  </div>
+  <div className="badge-container grow">
+    <a
+      href="https://buildspace.so/builds/ai-avatar"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div className="badge">
+        <Image src={buildspaceLogo} alt="buildspace logo" />
+        <p>build with buildspace</p>
+      </div>
+    </a>
+  </div>
+</div>
   );
 };
 
