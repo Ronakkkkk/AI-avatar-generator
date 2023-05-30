0ed00ee44ded35713b7bfa062674da4e4c9ddb2f
Ronak <ronak01.raj2gmail.com>
2023-05-30 12:39:23 +0530

    api and some ui

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+const generateAction = async (req, res) => {
+    console.log('Received request')
+    const input = JSON.parse(req.body).input;
+    const response = await fetch(
+        `https://api-inference.huggingface.co/models/Ronak0101/momo`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: JSON.stringify({
+            inputs: input,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        const buffer = await response.arrayBuffer();
+        res.status(200).json({ image: buffer });
+      } else if (response.status === 503) {
+        const json = await response.json();
+        res.status(503).json(json);
+      } else {
+        const json = await response.json();
+        res.status(response.status).json({ error: response.statusText });
+      }
+  }
+  
+  export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,67 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const maxRetries = 20;
+  const [input, setInput] = useState('');
+  const [img, setImg] = useState(''); 
+  const [retry, setRetry] = useState(0);
+  // Number of retries left
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+  const generateAction = async () => {
+    console.log('Generating...');	
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+  
+    const data = await response.json();
+
+    if (response.status === 503) {
+      console.log('Model is loading still :(.')
+      return;
+    }
+  
+    // If another error, drop error
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      return;
+    }
+
+    setImg(data.Image);
+  }
+  
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>Silly Picture Generator</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Silly picture generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Turn me into anyone you want (hulk please!) Make sure you refer to me as "ronka" in the prompt</h2>
           </div>
+          <div className="prompt-container">
+          <input className="prompt-box" value={input} onChange={onChange}/>
+          <div className="prompt-buttons">
+    <a className="generate-button">
+      <div className="generate" onClick={generateAction}>
+        <p>Generate</p>
+      </div>
+    </a>
+    </div>
+      </div>
         </div>
       </div>
       <div className="badge-container grow">
@@ -24,10 +70,10 @@ const Home = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <div className="badge">
+          {/* <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
             <p>build with buildspace</p>
-          </div>
+          </div> */}
         </a>
       </div>
     </div>
